8c7d2a57943d77b3d193c0c94188272d05afca2e
haldokoko <haldokoko@gmail.com>
2022-06-11 11:12:03 +0800

    bismillah

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +19,26 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
-
+const bacaData = (callback) => {
+const hasil = [];
+fs.readFile(file1, 'utf8', function read(err,data){
+    let konten = JSON.parse(data)
+    message = konten.message;
+    hasil.push(message.split(' ')[1])
+    fs.readFile(file2, 'utf8',function read(err,data) {
+        konten = JSON.parse(data)
+        message = konten[0].message;
+        hasil.push(message.split(' ')[1])
+        fs.readFile(file3,'utf8', function read(err,data) {
+          konten = JSON.parse(data)
+          message = konten[0].data.message;
+          hasil.push(message.split(' ')[1])
+          console.log(hasil)
+          callback(err, hasil)
+        })
+      })
+  })
+}
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
